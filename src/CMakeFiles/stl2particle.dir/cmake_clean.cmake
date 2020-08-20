273fef3002cba4e10b8c682c6b4b400153ef534e
Chiang-Yuan <qaqowotut@gmail.com>
2020-08-20 15:11:39 +0800

    build

---
+++ src/CMakeFiles/stl2particle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stl2particle.dir/main.cpp.o"
+  "stl2particle"
+  "stl2particle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stl2particle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
