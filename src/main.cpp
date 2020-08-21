10948b4b62359f6b083353e62dc1bb76a89c7b4e
Chiang-Yuan <qaqowotut@gmail.com>
2020-08-21 17:14:44 +0800

    lattice

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ Yuan Chiang (20th/Aug/2020)
 #include <vector>
 #include <string>
 #include <stdio.h>
+#include <math.h>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
@@ -17,6 +18,8 @@ Yuan Chiang (20th/Aug/2020)
 #include <CGAL/Extreme_points_traits_adapter_3.h>
 
 #include <CGAL/Vector_3.h>
+#include <CGAL/Point_set_3.h>
+#include <CGAL/Point_set_3/IO.h>
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/convex_hull_3.h>
@@ -24,6 +27,7 @@ Yuan Chiang (20th/Aug/2020)
 #include <CGAL/AABB_traits.h>
 #include <CGAL/AABB_triangle_primitive.h>
 #include <CGAL/AABB_face_graph_triangle_primitive.h>
+
 
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
@@ -39,6 +43,9 @@ typedef K::Triangle_3 Triangle;
 typedef CGAL::Polyhedron_3<K> Polyhedron;
 
 typedef CGAL::Surface_mesh<Point> Mesh;
+
+typedef CGAL::Point_set_3<Point> Point_set;
+typedef Point_set::Property_map<int> Type_map;
 
 typedef std::vector<Point> Points;
 typedef std::list<Triangle>::iterator Iterator;
@@ -64,6 +71,15 @@ int main(int argc, char* argv[])
     printf("\tNumber of edges    %10d\n", mesh.number_of_edges());
     printf("\tNumber of faces    %10d\n", mesh.number_of_faces());
 
+    // Vector phase()
+
+    // Point origin(0.0, 0.0, 0.0);
+
+    double lc = 1;
+    Vector fcc_a1(0.0, 0.5, 0.5);
+    Vector fcc_a2(0.5, 0.0, 0.5);
+    Vector fcc_a3(0.5, 0.5, 0.0);
+
     printf("Computing convex hull...\n");
     // define polyhedron to hold convex hull
     Mesh convex_hull;
@@ -71,6 +87,7 @@ int main(int argc, char* argv[])
     // access the vertices of
     Points vertices;
     double xc, yc, zc;
+
     Mesh::Property_map<vertex_descriptor, Point> coord = mesh.points();
     for(vertex_descriptor vd : mesh.vertices()) {
       vertices.push_back(coord[vd]);
@@ -78,21 +95,39 @@ int main(int argc, char* argv[])
       yc = yc + coord[vd][1];
       zc = zc + coord[vd][2];
     }
-    xc/=mesh.number_of_vertices();
-    yc/=mesh.number_of_vertices();
-    zc/=mesh.number_of_vertices();
-    Point centroid(xc, yc, zc);
+    Point centroid(xc/=mesh.number_of_vertices(),
+                   yc/=mesh.number_of_vertices(),
+                   zc/=mesh.number_of_vertices());
 
-    // std::cout << "The centroid of vertices is located at " << centroid <<  std::endl;
+    // int n_a1, n_a2, n_a3;
+    int n_a1 = 0;
+    int n_a2 = 0;
+    int n_a3 = 0;
+    for(vertex_descriptor vd : mesh.vertices()) {
+      Vector vertex(centroid, coord[vd]);
+      if (abs((vertex*fcc_a1)/(fcc_a1*fcc_a1)) > n_a1)
+        n_a1 = (int)ceil(abs((vertex*fcc_a1)/(fcc_a1*fcc_a1)));
+      if (abs((vertex*fcc_a2)/(fcc_a2*fcc_a2)) > n_a2)
+        n_a2 = (int)ceil(abs((vertex*fcc_a2)/(fcc_a2*fcc_a2)));
+      if (abs((vertex*fcc_a3)/(fcc_a3*fcc_a3)) > n_a3)
+        n_a3 = (int)ceil(abs((vertex*fcc_a3)/(fcc_a3*fcc_a3)));
+    }
+
 
     // compute convex hull of non-collinear points
     CGAL::convex_hull_3(vertices.begin(), vertices.end(), convex_hull);
-    std::cout << "\tThe convex hull contains " << convex_hull.number_of_vertices() << " vertices" << std::endl;
+
+    printf("\tNumber of vertices %10d\n", convex_hull.number_of_vertices());
+    printf("\tNumber of edges    %10d\n", convex_hull.number_of_edges());
+    printf("\tNumber of faces    %10d\n", convex_hull.number_of_faces());
+
+    std::cout << "\tCentroid of vertices is located at " << centroid << std::endl;
+    printf("\tNumber of lattices %3d %3d %3d\n", 2*n_a1+1, 2*n_a2+1, 2*n_a3+1);
 
     // export convex hull
     std::ofstream output("output/convex_hull.off");
     output << convex_hull;
-    
+
     // //This will contain the extreme vertices
     // std::vector<Mesh::Vertex_index> extreme_vertices;
     // //call the function with the traits adapter for vertices
@@ -101,10 +136,34 @@ int main(int argc, char* argv[])
     // //print the number of extreme vertices
     // std::cout << "There are  " << extreme_vertices.size() << " extreme vertices in this mesh." << std::endl;
 
-    double lc = 1.0;
-    Vector fcc_a1(0.0, 0.5, 0.5);
-    Vector fcc_a2(0.5, 0.0, 0.5);
-    Vector fcc_a3(0.5, 0.5, 0.0);
+    Point_set particles;
+    Type_map type;
+
+    bool success = false;
+    boost::tie (type, success) = particles.add_property_map<int> ("type", 0);
+    assert(success);
+
+    // particles.reserve (10); // For memory optimization
+    // for (std::size_t i = 0; i < 10; ++ i)
+    // {
+    //   Point_set::iterator it = point_set.insert (Point (double(i), double(i), double(i)));
+    //   Color c = {{ (unsigned char)(CGAL::get_default_random().get_int(0, 255)),
+    //                (unsigned char)(CGAL::get_default_random().get_int(0, 255)),
+    //                (unsigned char)(CGAL::get_default_random().get_int(0, 255)) }};
+    //   color[*it] = c;
+    //   intensity[*it] = rand() / (double)(RAND_MAX);
+    // }
+
+    for (int i = -n_a1; i <= n_a1; i++){
+      for (int j = -n_a2; j <= n_a2; j++){
+        for (int k = -n_a3; k <= n_a3; k++){
+          Point_set::iterator it = particles.insert(centroid + i*fcc_a1 + j*fcc_a2 + k*fcc_a3);
+          type[*it] = 1;
+        }
+      }
+    }
+    std::ofstream outxyz("output/particle.xyz");
+    CGAL::write_xyz_point_set(outxyz, particles);
 
     Vector epsilon(1.0,1.0,1.0);
 
